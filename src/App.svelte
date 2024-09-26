156c73b1e0e87b9f0b66f52ad9f5afde6dc1db7c
GustavoJCL <gjcalizayal@unjbg.edu.pe>
2024-09-26 05:06:00 -0500

    fix: fix some issues with tailwind css

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
   import Counter from "./lib/Counter.svelte";
 </script>
 
-<h1 class="text-3xl font-bold underline">
-  <div class="bg-red-600">Himmler is based</div>
+<h1 class="text-3xl font-bold underline flex-row">
+  <div class="bg-red-600 text-sky-900 text-4xl">Himmler is based</div>
   Hello world!
 </h1>
